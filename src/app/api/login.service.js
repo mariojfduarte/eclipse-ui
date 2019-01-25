99692b7458ec2f2dad18f184355d4a78b182d273
Mario Duarte <mariojfonseca.duarte@gmail.com>
2019-01-25 15:48:13 +0000

    Minor changes

--- src/app/api/login.service.js
+++ src/app/api/login.service.js
@@ -87,7 +87,7 @@ function LoginService($http, $q) {
 
     function activate(activateToken, password) {
         var deferred = $q.defer();
-        var url = 'http://www.eclipse-iot.com:9090/api/noauth/activate';
+        var url = '/api/noauth/activate';
         $http.post(url, {activateToken: activateToken, password: password}).then(function success(response) {
             deferred.resolve(response);
         }, function fail() {
